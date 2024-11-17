9b085c8f771db9cea3ea30e198a2165322419d08
kpauner <82957241+kpauner@users.noreply.github.com>
2024-11-17 02:01:10 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import createApp from "@/lib/create-app";
 import configureOpenAPI from "./lib/configure-open-api";
 import index from "./routes/index.route";
   import tasks from "./routes/tasks/tasks.index";
+import { cors } from "hono/cors";
 
 const app = createApp();
+
+app.use('/*', cors({
+  origin: ['http://localhost:3000'], // or whatever your frontend URL is
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}))
 
 const routes = [
   index,
